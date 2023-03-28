7e5a6d60e448ef5bf0b0d9ad9ce2d6952685425e
Adam Kulesza <adam.kule@gmail.com>
2023-03-28 23:47:32 +0100

    Uses shell now

--- models/imu_box.py
+++ models/imu_box.py
@@ -10,7 +10,7 @@ prongs_length = pcb_size[2] + prongs_stickout
 prongs_radious = holes_radious*2 - 0.1
 
 box_thickness = 1.2
-box_height = pcb_size[2] + prongs_length
+box_height = prongs_length + box_thickness
 
 socket_length = box_height - pcb_size[2] - box_thickness
 socket_radious = holes_radious + 1.25
@@ -21,23 +21,16 @@ box_depth = box_thickness*2 + holes_rect[1] + socket_radious*4 + 1.5
 
 bottom = (cq
   .Workplane('XY')
-  .box(box_width,box_depth,box_height)        
-  .faces('>Z').workplane()
-  .rect(box_width - box_thickness*2,box_depth - box_thickness*2)
-  .extrude(-box_height+box_thickness,'cut')
-  .faces('<<Z[-2]').workplane()
+  .box(box_width,box_depth,box_height-box_thickness)        
+  .faces('+Z')
+  .shell(box_thickness)
+  .faces('<Z[-2]').workplane()
   .rect(*holes_rect)
   .vertices()
   .cylinder(prongs_length,prongs_radious,centered=[True,True,False])
 )
 
-cut = (cq.Workplane('front')
- #.faces('<Z').workplane(centerOption='CenterOfMass')
- #.rect(port_width,box_height-pcb_size[2])
- #.extrude(-1,'cut')
-       )
 
-
-#cont = (bottom
-#)
-#debug(cont)
+ #.faces('>Z').workplane()
+ #.rect(box_width - box_thickness*2,box_depth - box_thickness*2)
+ #.extrude(-box_height+box_thickness,'cut')
