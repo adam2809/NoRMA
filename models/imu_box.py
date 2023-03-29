07f605d259b53b148c889162e7c19e0bdabd8971
Adam Kulesza <adam.kule@gmail.com>
2023-03-29 01:01:35 +0100

    Adds hex holes

--- models/imu_box.py
+++ models/imu_box.py
@@ -9,7 +9,7 @@ prongs_stickout = 5
 prongs_length = pcb_size[2] + prongs_stickout
 prongs_radious = holes_radious*2 - 0.1
 
-box_thickness = 1.2
+box_thickness = 2.8
 box_height = prongs_length + box_thickness
 
 socket_length = box_height - pcb_size[2] - box_thickness
@@ -21,7 +21,25 @@ box_depth = box_thickness*2 + holes_rect[1] + socket_radious*4 + 1.5
 
 port_slot = (cq
   .Workplane('XZ',(0,box_depth/2,0))
-  .box(port_width,box_height-pcb_size[2],box_thickness*3)
+  .box(port_width,box_height-box_thickness,box_thickness*3)
+)
+nut_cutouts = (cq
+  .Workplane('XY',(0,box_depth/2,0))
+  .box(port_width,box_height-box_thickness,box_thickness*3)
+)
+
+hex_holes = (cq
+  .Workplane('XY',(0,0,0))
+  .sketch()
+  .push([
+      (holes_rect[0]/2,holes_rect[1]/2),
+      (-holes_rect[0]/2,-holes_rect[1]/2),
+      (holes_rect[0]/2,-holes_rect[1]/2),
+      (-holes_rect[0]/2,holes_rect[1]/2)
+  ])
+  .regularPolygon(3.05,6,90)
+  .finalize()
+  .extrude(2.1)
 )
 bottom = (cq
   .Workplane('XY')
@@ -31,10 +49,21 @@ bottom = (cq
   .faces('<Z[-2]').workplane()
   .rect(*holes_rect,forConstruction=True)
   .vertices()
-  .cylinder(prongs_length,prongs_radious,centered=[True,True,False])
+  .hole(holes_radious+0.2)
 )
 cont = (bottom
   .cut(port_slot)
+  .faces("<Z")
+  .sketch()
+  .push([
+      (holes_rect[0]/2,holes_rect[1]/2),
+      (-holes_rect[0]/2,-holes_rect[1]/2),
+      (holes_rect[0]/2,-holes_rect[1]/2),
+      (-holes_rect[0]/2,holes_rect[1]/2)
+  ])
+  .regularPolygon(3.05,6,90)
+  .finalize()
+  .cutBlind(2.1)
 )
 show_object(cont)
  #.faces('>Z').workplane()
