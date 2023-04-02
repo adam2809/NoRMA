6d49feb2c176f4ab9805c67b5a2c214aeb8ddb88
Adam Kulesza <adam.kule@gmail.com>
2023-04-02 01:07:46 +0100

    Adds washers and using split and shell

--- models/imu_box.py
+++ models/imu_box.py
@@ -10,6 +10,7 @@ prongs_length = pcb_size[2] + prongs_stickout
 prongs_radious = holes_radious*2 - 0.1
 
 nut_thickness = 2
+nut_r = 3
 bolt_shaft_length = 18
 
 box_thickness = 2.8
@@ -22,10 +23,15 @@ socket_bore_radious = holes_radious
 box_width = box_thickness*2 + holes_rect[0] + socket_radious*4 + 1.5
 box_depth = box_thickness*2 + holes_rect[1] + socket_radious*4 + 1.5
 
+box_width_i = box_width - box_thickness*2
+box_depth_i = box_depth - box_thickness*2
+box_height_i = box_height - box_thickness*2
+
+
 
 port_slot = (cq
   .Workplane('XZ',(0,box_depth/2,0))
-  .box(port_width,box_height-box_thickness,box_thickness*3)
+  .box(port_width,box_height_i,box_thickness*3)
 )
 nut_cutouts = (cq
   .Workplane('XY',(0,box_depth/2,0))
@@ -41,14 +47,13 @@ hex_holes = (cq
       (holes_rect[0]/2,-holes_rect[1]/2),
       (-holes_rect[0]/2,holes_rect[1]/2)
   ])
-  .regularPolygon(3.05,6,90)
+  .regularPolygon(nut_r+0.1,6,90)
   .finalize()
-  .extrude(2.1)
+  .extrude(nut_thickness+0.1)
 )
-bottom = (cq
-  .Workplane('XY',(0,0,-10))
-  .box(box_width,box_depth,box_height-box_thickness)        
-  .faces('+Z')
+box = (cq
+  .Workplane('XY',(0,0,0))
+  .box(box_width_i,box_depth_i,box_height_i)        
   .shell(box_thickness)
   .faces('<Z[-2]').workplane()
   .rect(*holes_rect,forConstruction=True)
@@ -66,21 +71,28 @@ bottom = (cq
   .regularPolygon(3.05,6,90)
   .finalize()
   .cutBlind(nut_thickness+0.1)
-)
-
-top = (cq
-  .Workplane('XY')
-  .box(box_width,box_depth,box_thickness)        
   .faces('>Z')
   .rect(*holes_rect,forConstruction=True)
   .vertices()
   .cboreHole(holes_radious*2+0.1,6,box_thickness/2)
-  .edges('>Z and (>>X[-1] or >>Y[-1] or >>X[0] or >>Y[0])')
-  .fillet(box_thickness/2-0.001)
+)
+(lid, bottom) = box.faces(">Z").workplane(-box_thickness).split(keepTop=True, keepBottom=True).all()  # splits into two solids
+(washers,lid) = (lid
+  .faces("<Z").workplane()
+  .rect(*holes_rect,forConstruction=True)
+  .vertices()
+  .cylinder(box_height_i-pcb_size[2]-0.1, nut_r,centered=[True,True,False])
+  .faces("<Z").workplane()
+  .rect(*holes_rect,forConstruction=True)
+  .vertices()
+  .hole(holes_radious*2)
+  .faces("<Z[-2]")
+  .split(keepTop=True, keepBottom=True).all()  # splits into two solids
 )
 
-show_object(top)
 show_object(bottom)
+#show_object(lid)
+show_object(washers)
  #.faces('>Z').workplane()
  #.rect(box_width - box_thickness*2,box_depth - box_thickness*2)
  #.extrude(-box_height+box_thickness,'cut')
