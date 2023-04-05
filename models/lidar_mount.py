df8c9dc96f779e4c90d1e2511df0ee85e06dbd04
Adam Kulesza <adam.kule@gmail.com>
2023-04-05 16:50:24 +0100

    Adds lidar mount

---
+++ models/lidar_mount.py
@@ -0,0 +1,56 @@
+
+lidar_box_width = 50
+lidar_box_depth = 50
+lidar_box_height = 41
+
+rail_mount_ir = 35/2
+rail_mount_er = 38.4/2
+rail_mount_length = 55
+
+internal_diagonal_holes = 54 
+external_diagonal_holes = 59
+
+
+screw_head_radious = 3.5
+screw_hole_radious = 2
+cbore_length = 1
+
+
+#screw hole to end of stuff 94
+
+screw_hole = (cq
+  .Workplane('XY')
+  .cylinder(1000,screw_hole_radious)
+)
+
+cbore = (cq
+  .Workplane('XY',origin=(0,0,-(rail_mount_er)))
+  .cylinder(cbore_length,screw_head_radious,centered=[True,True,False])
+)
+
+lidar_attachment = (cq
+  .Workplane('XY')
+  .box(rail_mount_er+16,rail_mount_length,rail_mount_er*2,centered=[False,True,True])
+)
+
+cutoff_bottom = (cq
+  .Workplane()
+  .box(rail_mount_er*2,rail_mount_length,rail_mount_er,centered=[True,True,False])
+)
+
+cutoff_middle = (cq
+  .Workplane('ZX')
+  .cylinder(rail_mount_length,rail_mount_ir)
+)
+
+rail_mount = (cq
+  .Workplane('XZ')
+  .cylinder(rail_mount_length,rail_mount_er)
+) + lidar_attachment - screw_hole - cutoff_bottom -cutoff_middle - cbore
+with_holes = (rail_mount
+  .faces('>X').workplane(centerOption='CenterOfMass')
+  .pushPoints([[-rail_mount_length/3,0],[rail_mount_length/3,0]])
+  .hole(3,10)
+)
+show_object(with_holes)
+#debug(cbore)
---
+++ models/mounting_rail.py
@@ -0,0 +1,28 @@
+rail_height = 33
+rail_depth = rail_height
+
+clamp_width = 40
+clamp_thick = 4
+clamp_height = clamp_thick*2 + rail_height
+clamp_depth = clamp_thick*2 + rail_depth
+bolt_piece_depth = 5.8
+bolt_piece_width = 10 
+
+clamp_whole = (cq
+  .Workplane('XY')
+  .box(clamp_width,clamp_depth,clamp_height)
+  .faces('+X').workplane()
+  .rect(rail_height,rail_depth)
+  .cutThruAll()
+)
+#show_object(clamp_whole)
+cont = (clamp_whole
+  .faces('>Z').workplane(centerOption='CenterOfMass')
+  .pushPoints([
+    [(clamp_width-bolt_piece_width/2)/2,0],
+    [-(clamp_width-bolt_piece_width/2)/2,0]
+  ])
+  .rect(bolt_piece_width/2,bolt_piece_depth*2)
+)
+
+show_object(cont)
