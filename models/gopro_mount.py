2b654e7414cd666dd260984cb103ed3dc345b06b
Adam Kulesza <adam.kule@gmail.com>
2023-04-09 17:23:46 +0100

    Adds nut holder

--- models/gopro_mount.py
+++ models/gopro_mount.py
@@ -5,18 +5,33 @@ outer_radius = 14.88/2
 slot_depth = 15.66
 base_height = 15
 hole_height = 10
+nut_diameter = 9
+nut_thickness = 4
+nut_holder_r = (nut_diameter + 2.4)/2
 
-def mount_part(wp):
-    return (wp
+def mount_part(wp,is_nut):
+    res = (wp
       .box(outer_radius*2,thickness,base_height,centered=[True,True,False])
       .faces('+Y').workplane(centerOption='CenterOfMass',offset=-thickness)
       .center(0,base_height/2)
       .cylinder(thickness,outer_radius,centered=[True,True,False])
-      .faces('+Y').workplane(centerOption='CenterOfBoundBox')
+      .faces('>Y').workplane(centerOption='CenterOfBoundBox')
       .center(0,-(base_height+outer_radius)/2+base_height)
-      .hole(hole_r)
     )
+    if is_nut:
+      res = (res
+        .cylinder(nut_thickness,nut_holder_r)
+        .faces('>Y').workplane(centerOption='CenterOfBoundBox')
+        .polygon(6,nut_diameter)
+        .cutBlind(-nut_thickness)
+        .faces('>Y').workplane(centerOption='CenterOfBoundBox')
+        .hole(hole_r*2)
+      )
+    else:
+      res = res.hole(hole_r*2)
 
+    res = res
+    return res
 #mount = (cq
 #  .Workplane('XY')
 #  .box(outer_radius*2,thickness,base_height,centered=[True,True,False])
@@ -30,12 +45,11 @@ def mount_part(wp):
 parts = []
 origin_y = -(gap+thickness)
 
-for i in range(3):
-    mount = (cq
-      .Workplane('XY',origin=(0,origin_y,0))
-    )
-    mount = mount_part(mount)
-    origin_y += gap+thickness
-    
-    show_object(mount)
+mount = (cq
+  .Workplane('XY',origin=(0,0,0))
+)
+mount = mount_part(mount,False)
+origin_y += gap+thickness
+
+show_object(mount)
 
