3fef59f5d6effffd2ab72868487f1970c66dac14
Adam Kulesza <adam.kule@gmail.com>
2023-04-17 01:05:19 +0100

    Converts to collecting odometry

--- norma_nav/src/scan_matching_vel.py
+++ norma_nav/src/scan_matching_vel.py
@@ -3,38 +3,14 @@
 import rospy
 import math
 import numpy as np
-from geometry_msgs.msg import Pose2D, Twist
-
-#def get_orthogonal_rotation_matrix():
-#    mat = ""
-#    mat += f"{math.cos(t)} {math.sin(t)} 0 ;}"
-#    mat += f"{-math.cos(t)} {-math.sin(t)} 0 ;}"
-#    mat += f"{0 0 1}"
-#    return np.matrix(mat)
-prev = Pose2D()
-prev.x = 0
-prev.y = 0
-prev.theta = 0
-prev_time = 0.0
+from geometry_msgs.msg import TwistWithCovariance,Twist
+from nav_msgs.msg import Odometry
 
 curr_x = 0
 f = open('test.csv','w')
-def pose_cb (msg):
-    global prev_time
-    x_diff = msg.x - prev.x
-    y_diff = msg.y - prev.y
-    theta_diff = msg.theta - prev.theta
-    
-    theta_no_zero = 0.001 if msg.theta == 0 else msg.theta
-    time_diff = rospy.get_time()-prev_time
-    linear_vel = math.sqrt(x_diff**2 + y_diff**2)/time_diff
-    angular_vel = theta_diff/time_diff
-
-    prev.x = msg.x
-    prev.y = msg.y
-    prev.theta = msg.theta
-    prev_time = rospy.get_time()
-    f.write("{} , {}\n".format(linear_vel,curr_x))
+def odom_cb (msg):
+    linear_vel = msg.twist.twist.linear.x
+    f.write("{} ,{} , {}\n".format(msg.header.stamp,linear_vel,curr_x))
 
 
 def cmd_vel_cb(msg):
@@ -48,7 +24,7 @@ def close_file():
 rospy.init_node('scan_matching_vel')
 
 
-sub = rospy.Subscriber('/pose2D', Pose2D, pose_cb)
+sub = rospy.Subscriber('/odometry/filtered_map', Odometry, odom_cb)
 sub = rospy.Subscriber('/cmd_vel', Twist, cmd_vel_cb)
 rospy.on_shutdown(close_file)
 rospy.spin()
